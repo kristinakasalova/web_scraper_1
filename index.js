471b3b58f27b9194b4edf07f1fca9cd0dce3cbd7
kristinakasalova <125463086+kristinakasalova@users.noreply.github.com>
2024-08-24 21:51:51 +0000

    dded json.stringify and fs.writeFile functions in order to write the results into a separate .json fileadded json.stringify and fs.writeFile functions in order to write the results into a separate .json file

--- index.js
+++ index.js
@@ -3,10 +3,14 @@ const axios = require('axios');
 const cheerio = require('cheerio');
 const express = require('express');
 const cors = require('cors')
+const fs = require('fs');
+const { finished } = require('stream');
 
 const app = express();
 
 const url = 'https://www.europ-assistance.cz'
+const stringifiedData = [];
+let articles = [];
 
 app.use(cors())
 app.get('/', function(req, res){
@@ -19,12 +23,12 @@ app.get('/results', (req,res) => {
         const html = response.data
         //console.log(html)
         const $ = cheerio.load(html)
-        const articles = []
+        let articles = []
 
         $('.group', html).each(function() {
             const title = $(this).find('h3').text()
             const url = 'https://www.europ-assistance.cz'+$(this).attr('href')
-            const description = $(this).find('.text-ea-gray600').text()
+            const description = $(this).find('.text-ea-gray600').text().trim()
             const imageUrl = $(this).find('img').attr('src')
             articles.push({
                title,
@@ -35,9 +39,13 @@ app.get('/results', (req,res) => {
         })
         console.log(articles)
         res.json(articles)
+        data = JSON.stringify(articles, null, 2);
+        fs.writeFile('articles.json', data,finished);
+        function finished (err) {
+            console.log('problem')
+        }
     }).catch(err => console.log(err))
 })
-
 
 
 
